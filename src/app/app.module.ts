8b1ab71b383e7f2e150a92e1b12d004c97caf93b
Naima Salim <naimaayusuff@gmail.com>
2023-04-14 14:45:02 +0300

    Home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
+    
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  Header = 'DECORATE YOUR LIVING SPACE';
+  Heading = 'Shine a light';
+  sub_heading = 'Inside this beautifully-crafted jar, is a candle whose subtly blended fragrance helps to evoke the perfect world described by the American.';
+  last_title = 'See how it work';
+}
