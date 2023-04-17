5eb4459a1da6d68cb08480d347cddf4a74766539
Naima Salim <naimaayusuff@gmail.com>
2023-04-17 16:26:58 +0300

    Our Story

--- src/app/our-work/our-work.component.ts
+++ src/app/our-work/our-work.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./our-work.component.css']
 })
 export class OurWorkComponent {
+  title = 'What We Do';
+  sub_title = 'Treat yourself to a delightful scent combination (20 glorious hours!) of your choosing. Also works as a wonderful and personal gift for the candle lovers in your life. Whether they are a woodsy, floral, earthy, or citrus type, you can pick a set you know he or she will love.'
+  header = ' Design active temper'
+  sub_header = 'Treat yourself to a delightful scent combination (20 glorious hours!) of your choosing. Also works as a wonderful and personal gift for the candle lovers in your life. Whether they are a woodsy, floral, earthy, or citrus type, you can pick a set you know he or she will love.'
 
 }
